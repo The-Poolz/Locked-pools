ba4896fe4f2d200b876b8d2223d2949067826454
ashwinarora <ashwinarora48@gmail.com>
2021-02-28 17:26:03 +0530

    re-entry guards

--- contracts/ERC20Helper.sol
+++ contracts/ERC20Helper.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.24 <0.7.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract ERC20Helper {
     event TransferOut(uint256 Amount, address To, address Token);
@@ -11,23 +12,28 @@ contract ERC20Helper {
         address _owner,
         uint256 _amount
     ) {
-        if (ERC20(_token).allowance(_owner, address(this)) >= _amount) _;
-        else revert("No allownce");
-    }   
+        require(ERC20(_token).allowance(_owner, address(this)) >= _amount, "No allownce");
+        _;
+    }
     function TransferToken(
         address _Token,
         address _Reciver,
-        uint256 _ammount
+        uint256 _Amount
     ) internal {
-        emit TransferOut(_ammount, _Reciver, _Token);
-        ERC20(_Token).transfer(_Reciver, _ammount);
+        uint256 OldBalance = CheckBalance(_Token, address(this));
+        emit TransferOut(_Amount, _Reciver, _Token);
+        ERC20(_Token).transfer(_Reciver, _Amount);
+        assert((SafeMath.add(CheckBalance(_Token, address(this)), _Amount)) == OldBalance);
     } 
     function CheckBalance(address _Token,address _Subject) internal view returns(uint256) {
-          return ERC20(_Token).balanceOf(_Subject);
+        return ERC20(_Token).balanceOf(_Subject);
     }
     function TransferInToken(address _Token,address _Subject,uint256 _Amount) internal TestAllownce(_Token,_Subject,_Amount) {
+        require(_Amount > 0, "Amount should be greater than zero");
+        uint256 OldBalance = CheckBalance(_Token, address(this));
         ERC20(_Token).transferFrom(_Subject, address(this), _Amount);
         emit TransferIn(_Amount, _Subject, _Token);
+        assert(SafeMath.add(OldBalance, _Amount) == CheckBalance(_Token, address(this)));
     }
     function IsERC20(address _contractAddress) internal view returns (bool) {
         if (ERC20(_contractAddress).totalSupply() > 0) return true;
