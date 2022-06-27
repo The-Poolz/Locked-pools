a54136a84b9bbde38a021e0247e6c68c68bd3d40
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-06-27 16:47:58 +0300

    return allIds.length

--- contracts/LockedPoolzData.sol
+++ contracts/LockedPoolzData.sol
@@ -12,7 +12,7 @@ contract LockedPoolzData is LockedControl {
     // reconsider msg.sender
     function GetMyPoolsId() public view returns (uint256[] memory){
         uint256[] storage allIds = MyPoolz[msg.sender];
-        uint256[] memory ids = new uint256[](5);
+        uint256[] memory ids = new uint256[](allIds.length);
         uint256 index;
         for(uint i=0 ; i<allIds.length ; i++){
             if(AllPoolz[allIds[i]].StartAmount > AllPoolz[allIds[i]].DebitedAmount ){
