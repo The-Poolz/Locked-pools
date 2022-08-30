e1706b9c1574ea1f0f9e66bd594057d6da4cb78b
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-08-30 15:35:34 +0300

    splitting of creation logic

--- contracts/LockedControl.sol
+++ contracts/LockedControl.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./LockedPoolz.sol";
+import "./LockedCreation.sol";
 import "poolz-helper-v2/contracts/Array.sol";
 
-contract LockedControl is LockedPoolz {
+contract LockedControl is LockedCreation {
     function PoolTransfer(uint256 _PoolId, address _NewOwner)
         external
         isPoolValid(_PoolId)
@@ -57,71 +57,5 @@ contract LockedControl is LockedPoolz {
         uint256 _NewAmount = Allowance[_PoolId][msg.sender] - _Amount;
         Allowance[_PoolId][msg.sender]  = _NewAmount;
         return poolId;
-    }
-
-    function CreateNewPool(
-        address _Token, //token to lock address
-        uint256 _StartTime, //Until what time the pool will start
-        uint256 _FinishTime, //Until what time the pool will end
-        uint256 _StartAmount, //Total amount of the tokens to sell in the pool
-        address _Owner // Who the tokens belong to
-    ) external payable notZeroAddress(_Owner) returns(uint256) {
-        TransferInToken(_Token, msg.sender, _StartAmount);
-        if(WhiteList_Address != address(0) && !(isUserWithoutFee(msg.sender) || isTokenWithoutFee(_Token))){
-            PayFee(Fee);
-        }
-        CreatePool(_Token, _StartTime, _FinishTime, _StartAmount, _Owner);
-    }
-
-    function CreateMassPools(
-        address _Token,
-        uint256[] calldata _StartTime,
-        uint256[] calldata _FinishTime,
-        uint256[] calldata _StartAmount,
-        address[] calldata _Owner
-    )   external payable
-        isGreaterThanZero(_Owner.length)
-        isBelowLimit(_Owner.length)
-    {
-        require(_Owner.length == _FinishTime.length, "Date Array Invalid");
-        require(_StartTime.length == _FinishTime.length, "Date Array Invalid");
-        require(_Owner.length == _StartAmount.length, "Amount Array Invalid");
-        TransferInToken(_Token, msg.sender, Array.getArraySum(_StartAmount));
-        if(WhiteList_Address != address(0) && !(isUserWithoutFee(msg.sender) || isTokenWithoutFee(_Token))){
-            PayFee(Fee * _Owner.length);
-        }
-        uint256 firstPoolId = Index;
-        for(uint i=0 ; i < _Owner.length; i++){
-            CreatePool(_Token, _StartTime[i], _FinishTime[i], _StartAmount[i], _Owner[i]);
-        }
-        uint256 lastPoolId = Index - 1;
-        emit MassPoolsCreated(firstPoolId, lastPoolId);
-    }
-
-    // create pools with respect to finish time
-    function CreatePoolsWrtTime(
-        address _Token,
-        uint256[] calldata _StartTime,
-        uint256[] calldata _FinishTime,
-        uint256[] calldata _StartAmount,
-        address[] calldata _Owner
-    )   external payable
-        isGreaterThanZero(_StartTime.length)
-        isBelowLimit(_Owner.length * _FinishTime.length)
-    {
-        require(_Owner.length == _StartAmount.length, "Amount Array Invalid");
-        require(_FinishTime.length == _StartTime.length, "Date Array Invalid");
-        TransferInToken(_Token, msg.sender, Array.getArraySum(_StartAmount) * _FinishTime.length);
-        uint256 firstPoolId = Index;
-        if(WhiteList_Address != address(0) && !(isUserWithoutFee(msg.sender) || isTokenWithoutFee(_Token))){
-            PayFee(Fee * _Owner.length * _FinishTime.length);
-        }
-        for(uint i=0 ; i < _FinishTime.length ; i++){
-            for(uint j=0 ; j < _Owner.length ; j++){
-                CreatePool(_Token, _StartTime[i], _FinishTime[i], _StartAmount[j], _Owner[j]);
-            }
-        }
-        uint256 lastPoolId = Index - 1;
-        emit MassPoolsCreated(firstPoolId, lastPoolId);
     }
 }
