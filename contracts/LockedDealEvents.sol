a7d32e8f59620e46a69273eb512c1c24d0202daf
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-11-28 16:47:20 +0200

    events are indexed

--- contracts/LockedDealEvents.sol
+++ contracts/LockedDealEvents.sol
@@ -2,27 +2,27 @@
 pragma solidity ^0.8.0;
 
 contract LockedDealEvents {
-    event TokenWithdrawn(uint256 PoolId, address Recipient, uint256 Amount);
+    event TokenWithdrawn(uint256 PoolId, address indexed Recipient, uint256 Amount);
     event MassPoolsCreated(uint256 FirstPoolId, uint256 LastPoolId);
     event NewPoolCreated(
         uint256 PoolId,
-        address Token,
+        address indexed Token,
         uint256 StartTime,
         uint256 FinishTime,
         uint256 StartAmount,
-        address Owner
+        address indexed Owner
     );
     event PoolTransferred(
         uint256 PoolId,
         uint256 oldPoolId,
-        address NewOwner,
-        address OldOwner
+        address indexed NewOwner,
+        address indexed OldOwner
     );
-    event PoolApproval(uint256 PoolId, address Spender, uint256 Amount);
+    event PoolApproval(uint256 PoolId, address indexed Spender, uint256 Amount);
     event PoolSplit(
         uint256 OldPoolId,
         uint256 NewPoolId,
         uint256 NewAmount,
-        address NewOwner
+        address indexed NewOwner
     );
 }
