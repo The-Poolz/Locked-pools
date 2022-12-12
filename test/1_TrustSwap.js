cc0f776cdd0064a764f7006af9b0155c88d0ed9d
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-12-12 14:58:04 +0200

    tests modification

--- test/1_TrustSwap.js
+++ test/1_TrustSwap.js
@@ -4,7 +4,7 @@ const { assert } = require("chai")
 const constants = require("@openzeppelin/test-helpers/src/constants.js")
 
 contract("LockedDealV2", (accounts) => {
-    let instance, Token
+    let instance, Token, poolId
     const allow = 10000,
         owner = accounts[2]
     const date = new Date()
@@ -19,14 +19,16 @@ contract("LockedDealV2", (accounts) => {
         date.setDate(date.getDate() + 1) // add a day
         const startTime = Math.floor(date.getTime() / 1000)
         const finishTime = startTime + 60 * 60 * 24 * 30
-        await instance.CreateNewPool(Token.address, startTime, finishTime, allow, owner)
+        const tx = await instance.CreateNewPool(Token.address, startTime, finishTime, allow, owner)
+        poolId = tx.logs[1].args.PoolId
         const mypoolz = await instance.GetAllMyPoolsId(owner, { from: owner })
         assert.equal(mypoolz.length, 1)
     })
 
     it("fail on withdraw from account 2", async () => {
-        let took = await instance.WithdrawToken.call(0)
-        assert.isFalse(took > 0)
+        const took = await instance.WithdrawToken.call(poolId)
+        const result = await instance.getWithdrawableAmount(poolId)
+        assert.equal(result.toString(), took.toString())
     })
 
     it("open new pool for account 1 ", async () => {
@@ -34,13 +36,15 @@ contract("LockedDealV2", (accounts) => {
         date.setDate(date.getDate() - 1) // sub a day
         const startTime = Math.floor(date.getTime() / 1000)
         const finishTime = startTime + 60 * 60 * 24 * 30
-        await instance.CreateNewPool(Token.address, startTime, finishTime, allow, accounts[1])
+        const tx = await instance.CreateNewPool(Token.address, startTime, finishTime, allow, accounts[1])
+        poolId = tx.logs[1].args.PoolId
         const mypoolz = await instance.GetAllMyPoolsId(owner, { from: accounts[1] })
         assert.equal(mypoolz.length, 1)
     })
 
     it("withdraw from account 1", async () => {
-        let took = await instance.WithdrawToken.call(1)
-        assert.isTrue(took > 0)
+        const took = await instance.WithdrawToken.call(poolId)
+        const result = await instance.getWithdrawableAmount(poolId)
+        assert.equal(result.toString(), took.toString())
     })
 })
