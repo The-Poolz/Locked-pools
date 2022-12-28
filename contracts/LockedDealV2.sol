55dffb3df384272b93b2e4e00fd6d007f5ae24cb
Ashwin Arora <ashwinarora48@gmail.com>
2022-12-28 20:26:42 +0530

    Revert "changed TokenWithdrawn event (#146)"
    This reverts commit d8b47c70e8a8efb1a2e50327e04bb883a1ef993e.

--- contracts/LockedDealV2.sol
+++ contracts/LockedDealV2.sol
@@ -37,12 +37,10 @@ contract LockedDealV2 is LockedPoolzData {
             uint256 tempDebitAmount = withdrawnAmount + pool.DebitedAmount;
             pool.DebitedAmount = tempDebitAmount;
             TransferToken(pool.Token, pool.Owner, withdrawnAmount);
-            emit TokenWithdrawn(
-                _PoolId,
+            emit TokenWithdrawn(_PoolId,
                 pool.Owner,
-                withdrawnAmount,
-                remainingAmount(_PoolId)
-            );
+                withdrawnAmount,        
+                pool.StartAmount);
         }
     }
 }
