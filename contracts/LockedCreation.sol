dfec367394260f571d0127d6b232b55ed645fb7a
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-12-28 18:31:36 +0200

    fix build

--- contracts/LockedCreation.sol
+++ contracts/LockedCreation.sol
@@ -37,7 +37,7 @@ contract LockedCreation is LockedPoolz {
         require(_Owner.length == _FinishTime.length, "Date Array Invalid");
         require(_StartTime.length == _FinishTime.length, "Date Array Invalid");
         require(_Owner.length == _StartAmount.length, "Amount Array Invalid");
-        require(_CliffTime.length == _StartAmount.length, "CliffTime Array Invalid");
+        require(_CliffTime.length == _FinishTime.length,"CliffTime Array Invalid");
         TransferInToken(_Token, msg.sender, Array.getArraySum(_StartAmount));
         payFee(_Token, Fee * _Owner.length);
         uint256 firstPoolId = Index;
@@ -56,6 +56,8 @@ contract LockedCreation is LockedPoolz {
         emit MassPoolsCreated(firstPoolId, lastPoolId);
     }
 
+    event cliffTime(uint256 _amount, uint256 _amount2);
+
     // create pools with respect to finish time
     function CreatePoolsWrtTime(
         address _Token,
@@ -67,7 +69,7 @@ contract LockedCreation is LockedPoolz {
     ) external payable isBelowLimit(_Owner.length * _FinishTime.length) {
         require(_Owner.length == _StartAmount.length, "Amount Array Invalid");
         require(_FinishTime.length == _StartTime.length, "Date Array Invalid");
-        require(_CliffTime.length == _StartAmount.length, "CliffTime Array Invalid");
+        require(_CliffTime.length == _FinishTime.length, "CliffTime Array Invalid");
         TransferInToken(
             _Token,
             msg.sender,
