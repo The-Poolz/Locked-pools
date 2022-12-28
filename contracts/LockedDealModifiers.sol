c0dbb3c5ef2996ed116e96c829bbef20b2331c90
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-12-28 17:59:38 +0200

    remove ERC20 import

--- contracts/LockedDealModifiers.sol
+++ contracts/LockedDealModifiers.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
 /// @title contains modifiers and stores variables.
 contract LockedDealModifiers {
     mapping(uint256 => mapping(address => uint256)) public Allowance;
